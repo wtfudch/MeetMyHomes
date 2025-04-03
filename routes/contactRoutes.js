a7a005915b47affa03530a97af9556ca90ce0e4e
wtfudch <danilo79123@gmail.com>
2025-04-03 17:10:57 +0100

    Delete routes directory

--- routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/contactController');
-
-// Define routes
-router.get('/', contactController.getContactPage); // Change '/contact' to '/'
-router.post('/', contactController.submitContactForm); 
-
-module.exports = router;
--- routes/listingsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const listingsController = require('../controllers/listingController');
-
-// Define routes
-router.get('/', listingsController.getListingsPage); // Change '/listings' to '/'
-
-module.exports = router;
