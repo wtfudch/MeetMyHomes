a0466db439a6ff5bccc8fbb85ae033c5c0097489
wtfudch <danilo79123@gmail.com>
2025-04-10 16:49:21 +0100

    Fix server binding to 0.0.0.0

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '35.160.120.126', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
